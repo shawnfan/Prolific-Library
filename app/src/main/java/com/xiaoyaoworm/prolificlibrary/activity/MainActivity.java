327049591883ee40fe5ec404769c35487890bfd6
Leon Jiang <xiaoyaoworm@gmail.com>
2016-04-02 16:17:29 -0700

    find the problem why put and delete is not working correctly in my code

--- app/src/main/java/com/xiaoyaoworm/prolificlibrary/activity/MainActivity.java
+++ app/src/main/java/com/xiaoyaoworm/prolificlibrary/activity/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         listView = (ListView) findViewById(R.id.bookList);
 
-        refreshBooks();
+//        refreshBooks();
     }
 
     @Override
